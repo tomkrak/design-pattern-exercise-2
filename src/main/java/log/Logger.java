497149a83c8fd87e4a4559e24721430648a2dc41
tomkrak <tomasz.krakowiak@outlook.com>
2019-02-17 14:53:32 +0100

    commit bez sensu, spisane z notatek z LocationDAO

--- src/main/java/log/Logger.java
+++ src/main/java/log/Logger.java
@@ -4,4 +4,10 @@ public class Logger {
     public void log(String log) {
         System.out.println(log);
     }
+    public static Logger getInstance()  {
+        if (instance == null) {
+            return new Loger();
+            }
+    } return instance;
+
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import account.AccountService;
 import account.TransferService;
+import log.Logger;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,4 +13,5 @@ public class Main {
         accountService.withdraw("1212121212121212", 1000);
         transferService.transfer("1212121212121212","1212121212121213",500);
     }
+    Logger logger = Logger.getInstance();
 }
